fd5d592c3c162f91e9af75f15182ba29c3d784c0
kim-dongoh <dongoh31277@gmail.com>
2022-04-28 06:28:32 +0000

    init

--- scripts/helper/functions.sh
+++ scripts/helper/functions.sh
@@ -72,20 +72,6 @@ poststart_checks()
     echo -e "\nWARNING: at least one Docker container unexpectedly exited. Please troubleshoot, see https://docs.confluent.io/platform/current/tutorials/cp-demo/docs/troubleshooting.html"
   fi
 
-#  # Validate connectors are running
-#  connectorList=$(docker exec connect curl -s -X GET --cert /etc/kafka/secrets/connect.certificate.pem --key /etc/kafka/secrets/connect.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -u superUser:superUser https://connect:8083/connectors/ | jq -r @sh | xargs echo)
-#  for connector in $connectorList; do
-#    check_connector_status_running $connector || echo -e "\nWARNING: Connector $connector is not in RUNNING state. Is it still starting up?"
-#  done
-
-#  # Check number of Schema Registry subjects
-#  # The subject created by the Kafka Streams app may be created after start script ends, so ignore that subject here (to not add time to start script)
-#  numSubjects=6
-#  foundSubjects=$(docker exec schemaregistry curl -s -X GET --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -u superUser:superUser https://schemaregistry:8085/subjects | jq length)
-#  if [[ $foundSubjects -lt $numSubjects ]]; then
-#    echo -e "\nWARNING: Expected to find at least $numSubjects subjects in Schema Registry but found $foundSubjects subjects. Please troubleshoot, see https://docs.confluent.io/platform/current/tutorials/cp-demo/docs/troubleshooting.html"
-#  fi
-#
   echo
 }
 
@@ -110,9 +96,6 @@ clean_demo_env()
   # Remove existing keys and certificates
   (cd ${DIR}/../security && ./certs-clean.sh)
 
-  # Remove existing Connect image
-#  docker rmi -f localbuild/connect:${CONFLUENT_DOCKER_TAG}-${CONNECTOR_VERSION}
-
   # Remove existing tools image
   docker rmi -f localbuild/tools:${CONFLUENT_DOCKER_TAG}
 }
@@ -121,11 +104,6 @@ create_certificates()
 {
   local DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-#  # Generate keys and certificates used for SSL
-#  echo -e "Generate keys and certificates used for SSL (see ${DIR}/security)"
-#  # Install findutils to be able to use 'xargs' in the certs-create.sh script
-#  docker run -v ${DIR}/../security/:/etc/kafka/secrets/ -u0 $REPOSITORY/cp-server:${CONFLUENT_DOCKER_TAG} bash -c "yum -y install findutils; cd /etc/kafka/secrets && ./certs-create.sh && chown -R $(id -u $USER):$(id -g $USER) /etc/kafka/secrets"
-#  
   # Generating public and private keys for token signing
   echo "Generating public and private keys for token signing"
   docker run -v ${DIR}/../security/:/etc/kafka/secrets/ -u0 $REPOSITORY/cp-server:${CONFLUENT_DOCKER_TAG} bash -c "mkdir -p /etc/kafka/secrets/keypair; openssl genrsa -out /etc/kafka/secrets/keypair/keypair.pem 2048; openssl rsa -in /etc/kafka/secrets/keypair/keypair.pem -outform PEM -pubout -out /etc/kafka/secrets/keypair/public.pem && chown -R $(id -u $USER):$(id -g $USER) /etc/kafka/secrets/keypair"
@@ -151,64 +129,6 @@ build_tools_image()
   }
 }
 
-build_connect_image()
-{
-  echo
-  echo "Building custom Docker image with Connect version ${CONFLUENT_DOCKER_TAG} and connector version ${CONNECTOR_VERSION}"
-
-  local DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-  DOCKERFILE="${DIR}/../../Dockerfile"
-  CONTEXT="${DIR}/../../."
-  echo "docker build --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=$REPOSITORY -t localbuild/connect:${CONFLUENT_DOCKER_TAG}-${CONNECTOR_VERSION} -f $DOCKERFILE $CONTEXT"
-  docker build --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=$REPOSITORY -t localbuild/connect:${CONFLUENT_DOCKER_TAG}-${CONNECTOR_VERSION} -f $DOCKERFILE $CONTEXT || {
-    echo "ERROR: Docker image build failed. Please troubleshoot and try again. For troubleshooting instructions see https://docs.confluent.io/platform/current/tutorials/cp-demo/docs/troubleshooting.html"
-    exit 1
-  }
-
-  # Copy the updated kafka.connect.truststore.jks back to the host
-  docker create --name cp-demo-tmp-connect localbuild/connect:${CONFLUENT_DOCKER_TAG}-${CONNECTOR_VERSION}
-  docker cp cp-demo-tmp-connect:/tmp/kafka.connect.truststore.jks ${DIR}/../security/kafka.connect.truststore.jks
-  docker rm cp-demo-tmp-connect
-}
-
-build_viz()
-{
-  local DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-  echo
-  echo
-  echo "VIZ=true: running Elasticsearch, Elasticsearch sink connector, and Kibana"
-
-  docker-compose up --no-recreate -d elasticsearch kibana
-
-  # Verify Elasticsearch is ready
-  MAX_WAIT=240
-  echo
-  echo -e "\nWaiting up to $MAX_WAIT seconds for Elasticsearch to be ready"
-  retry $MAX_WAIT host_check_up elasticsearch || exit 1
-  echo -e "\nProvide data mapping to Elasticsearch:"
-  ${DIR}/../dashboard/set_elasticsearch_mapping_bot.sh
-  ${DIR}/../dashboard/set_elasticsearch_mapping_count.sh
-  echo
-
-  echo -e "\nStart streaming to Elasticsearch sink connector:"
-  ${DIR}/../connectors/submit_elastic_sink_config.sh || exit 1
-  echo
-
-  # Verify Kibana is ready
-  MAX_WAIT=120
-  echo
-  echo -e "\nWaiting up to $MAX_WAIT seconds for Kibana to be ready"
-  retry $MAX_WAIT host_check_up kibana || exit 1
-  echo -e "\nConfigure Kibana dashboard:"
-  ${DIR}/../dashboard/configure_kibana_dashboard.sh
-  echo
-
-  return 0
-}
-
-
 host_check_up()
 {
   containerName=$1
@@ -218,6 +138,7 @@ host_check_up()
   return 1
 }
 
+################################
 host_check_schema_registered()
 {
   FOUND=$(docker exec schemaregistry curl -s -X GET --cert /etc/kafka/secrets/schemaregistry.certificate.pem --key /etc/kafka/secrets/schemaregistry.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -u superUser:superUser https://schemaregistry:8085/subjects | grep "wikipedia.parsed-value")
@@ -226,7 +147,7 @@ host_check_schema_registered()
   fi
   return 0
 }
-
+#####################################
 
 mds_login()
 {
@@ -257,53 +178,4 @@ END
     echo "Failed to log into MDS. Please check all parameters and run again."
     exit
   fi
-}
-
-check_connector_status_running() {
-  connectorName=$1
-
-  STATE=$(docker exec connect curl -X GET --cert /etc/kafka/secrets/connect.certificate.pem --key /etc/kafka/secrets/connect.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -u superUser:superUser https://connect:8083/connectors/$connectorName/status 2>/dev/null | jq -r .connector.state)
-  if [[ "$STATE" != "RUNNING" ]]; then
-    return 1
-  fi
-  return 0
-}
-
-create_topic() {
-
-  local DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-  broker_host_port=$1
-  cluster_id=$2
-  topic_name=$3
-  confluent_value_schema_validation=$4
-  auth=$5
-
-  # note --tlsv1.2 below sets the _minimum_ allowed TLS version - expect TLS 1.3 to be negotiated here
-  {
-  IFS= read -rd '' out
-  IFS= read -rd '' http_code
-  IFS= read -rd '' status
-  } < <({ out=$(curl -sS -X POST \
-    -o /dev/stderr \
-    -w "%{http_code}" \
-    -u ${auth} \
-    --tlsv1.2 \
-    --cacert /etc/kafka/secrets/snakeoil-ca-1.crt \
-    --header 'Content-Type: application/json' \
-    --header 'Accept: application/json' \
-    --data-binary @<(jq -n --arg topic_name "${topic_name}" --arg confluent_value_schema_validation "${confluent_value_schema_validation}" -f ${DIR}/topic.jq) \
-    "https://${broker_host_port}/kafka/v3/clusters/${cluster_id}/topics"); } 2>&1; printf '\0%s' "$out" "$?") || true
-
-  #echo "response code: " $http_code
-  #echo $out| jq || true
-
-  if [[ $status -ne 0 || $http_code -gt 299 || -z $out || $out =~ "error_code" ]]; then
-    echo "ERROR: create topic failed $out"
-    return 1
-  else
-    echo "Created topic $topic_name"
-  fi
-
-  return 0
 }
--- scripts/start.sh
+++ scripts/start.sh
@@ -14,12 +14,10 @@ ${DIR}/stop.sh
 
 # Regenerate certificates and the Connect or tools Docker image if any of the following conditions are true
 if [[ "$CLEAN" == "true" ]] || \
-# ! [[ -f "${DIR}/security/controlCenterAndKsqlDBServer-ca1-signed.crt" ]] || \
-# ! [[ $(docker images --format "{{.Repository}}:{{.Tag}}" localbuild/connect:${CONFLUENT_DOCKER_TAG}-${CONNECTOR_VERSION}) =~ localbuild ]] || \
  ! [[ $(docker images --format "{{.Repository}}:{{.Tag}}" localbuild/tools:${CONFLUENT_DOCKER_TAG}) =~ localbuild ]] ;
 then
   if [[ -z $CLEAN ]] || [[ "$CLEAN" == "false" ]] ; then
-    echo "INFO: Setting CLEAN=true because minimum conditions not met (existing certificates, Connect Docker image localbuild/connect:${CONFLUENT_DOCKER_TAG}-${CONNECTOR_VERSION}), tools Docker image localbuild/tools:${CONFLUENT_DOCKER_TAG})"
+    echo "INFO: Setting CLEAN=true because minimum conditions not met (existing certificates, Connect tools Docker image localbuild/tools:${CONFLUENT_DOCKER_TAG})"
   fi
   CLEAN=true
   clean_demo_env
@@ -30,9 +28,7 @@ fi
 echo
 echo "Environment parameters"
 echo "  REPOSITORY=$REPOSITORY"
-echo "  CONNECTOR_VERSION=$CONNECTOR_VERSION"
 echo "  CLEAN=$CLEAN"
-echo "  VIZ=$VIZ"
 echo "  C3_KSQLDB_HTTPS=$C3_KSQLDB_HTTPS"
 echo
 
@@ -52,24 +48,10 @@ fi
 
 # Build custom tools image and connect image
 build_tools_image
-#if [[ "$CLEAN" == "true" ]] ; then
-#  build_connect_image || exit 1
-#fi
-
-# Check number of certificates
-#NUM_CERTS=$(docker-compose exec connect keytool --list --keystore /etc/kafka/secrets/kafka.connect.truststore.jks --storepass confluent | grep trusted | wc -l)
-#if [[ "$NUM_CERTS" -eq "1" ]]; then
-#  echo -e "\nERROR: Connect image did not build properly.  Expected ~147 trusted certificates but got $NUM_CERTS. Please troubleshoot and try again."
-#  exit 1
-#fi
 
 # Bring up tools
 docker-compose up --no-recreate -d tools
 
-## Add root CA to container (obviates need for supplying it at CLI login '--ca-cert-path')
-#docker-compose exec tools bash -c "cp /etc/kafka/secrets/snakeoil-ca-1.crt /usr/local/share/ca-certificates && /usr/sbin/update-ca-certificates"
-#
-#
 # Bring up base kafka cluster
 docker-compose up --no-recreate -d zookeeper kafka1 kafka2
 
@@ -96,32 +78,6 @@ docker-compose exec kafka1 kafka-configs \
 # Bring up more containers
 docker-compose up --no-recreate -d schemaregistry control-center
 
-#echo
-#echo -e "Create topics in Kafka cluster:"
-#docker-compose exec tools bash -c "/tmp/helper/create-topics.sh" || exit 1
-#
-## Verify Kafka Connect Worker has started
-#MAX_WAIT=240
-#echo -e "\nWaiting up to $MAX_WAIT seconds for Connect to start"
-#retry $MAX_WAIT host_check_up connect || exit 1
-
-#-------------------------------------------------------------------------------
-
-#echo -e "\nStart streaming from the Wikipedia SSE source connector:"
-#${DIR}/connectors/submit_wikipedia_sse_config.sh || exit 1
-#
-## Verify connector is running
-#MAX_WAIT=120
-#echo
-#echo "Waiting up to $MAX_WAIT seconds for connector to be in RUNNING state"
-#retry $MAX_WAIT check_connector_status_running "wikipedia-sse" || exit 1
-#
-## Verify wikipedia.parsed topic is populated and schema is registered
-#MAX_WAIT=120
-#echo
-#echo -e "Waiting up to $MAX_WAIT seconds for subject wikipedia.parsed-value (for topic wikipedia.parsed) to be registered in Schema Registry"
-#retry $MAX_WAIT host_check_schema_registered || exit 1
-
 #-------------------------------------------------------------------------------
 
 # Verify Confluent Control Center has started
@@ -133,17 +89,6 @@ retry $MAX_WAIT host_check_up control-center || exit 1
 echo -e "\nConfluent Control Center modifications:"
 ${DIR}/helper/control-center-modifications.sh
 echo
-
-
-## Register the same schema for the replicated topic wikipedia.parsed.replica as was created for the original topic wikipedia.parsed
-## In this case the replicated topic will register with the same schema ID as the original topic
-#echo -e "\nRegister subject wikipedia.parsed.replica-value in Schema Registry"
-#SCHEMA=$(docker exec schemaregistry curl -s -X GET --cert /etc/kafka/secrets/schemaregistry.certificate.pem --key /etc/kafka/secrets/schemaregistry.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -u superUser:superUser https://schemaregistry:8085/subjects/wikipedia.parsed-value/versions/latest | jq .schema)
-#docker-compose exec schemaregistry curl -X POST --cert /etc/kafka/secrets/schemaregistry.certificate.pem --key /etc/kafka/secrets/schemaregistry.key --tlsv1.2 --cacert /etc/kafka/secrets/snakeoil-ca-1.crt -H "Content-Type: application/vnd.schemaregistry.v1+json" --data "{\"schema\": $SCHEMA}" -u superUser:superUser https://schemaregistry:8085/subjects/wikipedia.parsed.replica-value/versions
-#
-#echo
-#echo -e "\nStart Confluent Replicator to loopback to on-prem cluster:"
-#${DIR}/connectors/submit_replicator_config.sh || exit 1
 
 #-------------------------------------------------------------------------------
 
@@ -157,26 +102,7 @@ MAX_WAIT=120
 echo -e "\nWaiting up to $MAX_WAIT seconds for ksqlDB server to start"
 retry $MAX_WAIT host_check_up ksqldb-server || exit 1
 
-#echo -e "\nRun ksqlDB queries:"
-#${DIR}/ksqlDB/run_ksqlDB.sh
-#
-#if [[ "$VIZ" == "true" ]]; then
-#  build_viz || exit 1
-#fi
-#
-#echo -e "\nStart additional consumers to read from topics WIKIPEDIANOBOT, WIKIPEDIA_COUNT_GT_1"
-#${DIR}/consumers/listen_WIKIPEDIANOBOT.sh
-#${DIR}/consumers/listen_WIKIPEDIA_COUNT_GT_1.sh
-#
-#echo
-#echo
-#echo "Start the Kafka Streams application wikipedia-activity-monitor"
-#docker-compose up --no-recreate -d streams-demo
-#echo "..."
-#
-
 #-------------------------------------------------------------------------------
-
 
 # Verify Docker containers started
 if [[ $(docker-compose ps) =~ "Exit 137" ]]; then
@@ -208,14 +134,6 @@ DONE! From your browser:
      $C3URL
 
 EOF
-
-#if [[ "$VIZ" == "true" ]]; then
-#cat << EOF
-#  Kibana
-#     $kibanaURL
-#
-#EOF
-#fi
 
 cat << EOF
 Want more? Learn how to replicate data from the on-prem cluster to Confluent Cloud:
